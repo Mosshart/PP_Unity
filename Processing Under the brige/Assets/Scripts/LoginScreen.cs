ab6ee0db0987fd498535099d07cffa826d81a506
Filip Mystek <filip.mystek@gmail.com>
2019-03-13 13:06:01 +0100

    project push/ firs ui prototype

---
+++ Processing Under the brige/Assets/Scripts/LoginScreen.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Login : MonoBehaviour
+{
+
+    InputField login;
+    InputField password;
+    public Canvas mainCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void onLoginClick()
+    {
+        //todo database check
+        if(login.text == "test" && password.text == "test")
+        {
+            gameObject.transform.parent.gameObject.SetActive(false);
+            mainCanvas.gameObject.SetActive(true);
+
+        }
+    }
+}
